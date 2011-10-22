f5dea6398eac59f4a5e6aff5d1ae482b4a71115a
Reed Underwood <reed.underwood@gmail.com>
2011-10-22 12:04:16 -0400

    test

--- lib/filethis/handler/main.php
+++ lib/filethis/handler/main.php
@@ -1,7 +1,13 @@
 <?php
 class Filethis_Handler_Main extends Filethis_Handler {
     public function get() {
-
+        header('Content-Type: application/xml');
+        $response = <<<EREIAM
+<?xml version="1.0" encoding="UTF-8"?>
+    <Response>
+        <Say>Hello, Monkey.</Say>
+    </Response>
+EREIAM;
     }
 
     public function post() {
